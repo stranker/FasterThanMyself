6e3d30bc006baf86ffe27233ed5b662cc3f73093
Unknown <dani_mrx93@hotmail.com>
2017-11-10 12:52:50 -0300

    Add StateMachine

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,27 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var p:Player;
+	var platform:FlxSprite;
 	override public function create():Void
 	{
 		super.create();
+		p = new Player(FlxG.camera.width / 2, FlxG.camera.height / 2);
+		platform = new FlxSprite(150, 500);
+		platform.makeGraphic(500, 20, 0xFFFF00FF);
+		platform.immovable = true;
+		add(platform);
+		add(p);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		FlxG.collide(p, platform);
 		super.update(elapsed);
 	}
 }
---
+++ source/Player.hx
@@ -0,0 +1,125 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.addons.util.FlxFSM;
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+
+/**
+ * ...
+ * @author Daniel Natarelli
+ */
+class Player extends FlxSprite 
+{
+	private static inline var GRAVITY:Float = 1400;
+	private static inline var VEL:Int = 300;
+	private var fsm:FlxFSM<Player>;
+	
+	public function new(?X:Float=0, ?Y:Float=0) 
+	{
+		super(X, Y);
+		makeGraphic(32, 32, FlxColor.BLUE);
+		acceleration.y = GRAVITY;
+		setFacingFlip(FlxObject.LEFT, true, false);
+		setFacingFlip(FlxObject.RIGHT, false, false);
+		facing = FlxObject.RIGHT;
+		maxVelocity.set(VEL, GRAVITY);
+		
+		fsm = new FlxFSM<Player>(this);
+		fsm.transitions
+			.add(Idle, Jump, Conditions.jump)
+			.add(Jump, Idle, Conditions.grounded)
+			.add(Jump, Fall, Conditions.falling)
+			.add(Fall, Idle, Conditions.grounded)
+			.start(Idle);
+		
+		
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		fsm.update(elapsed);
+		super.update(elapsed);
+		trace(Type.getClassName(fsm.stateClass));
+	}
+	
+	public function movement():Void
+	{
+		if (FlxG.keys.pressed.LEFT || FlxG.keys.pressed.RIGHT)
+		{
+			velocity.x = FlxG.keys.pressed.LEFT ? -VEL : VEL;
+			// ANIMACION animation.play("run");
+		}
+		else
+		{
+			velocity.x = 0;
+			// ANIMACION animation.play("idle");
+		}
+		facing = (velocity.x > 0) ? FlxObject.RIGHT : FlxObject.LEFT;
+	}
+	
+	
+}
+
+class Conditions
+{
+	public static function jump(owner:Player):Bool
+	{
+		return (FlxG.keys.justPressed.UP && owner.isTouching(FlxObject.FLOOR));
+	}
+	
+	public static function grounded(owner:Player):Bool
+	{
+		return owner.isTouching(FlxObject.FLOOR);
+	}
+	
+	public static function falling(owner:Player):Bool
+	{
+		return (owner.velocity.y > 0 && !owner.isTouching(FlxObject.FLOOR));
+	}
+	
+	public static function animationFinished(owner:Player):Bool
+	{
+		return owner.animation.finished;
+	}
+}
+
+
+class Idle extends FlxFSMState<Player>
+{
+	override public function enter(owner:Player, fsm:FlxFSM<Player>):Void
+	{
+	}
+	override public function update(elapsed:Float, owner:Player, fsm:FlxFSM<Player>):Void 
+	{
+		owner.velocity.x = 0;
+		owner.movement();
+	}
+}
+
+class Jump extends FlxFSMState<Player>
+{
+	override public function enter(owner:Player, fsm:FlxFSM<Player>):Void
+	{
+		// animacion play (jump)
+		owner.velocity.y = -500;
+	}
+	override public function update(elapsed:Float, owner:Player, fsm:FlxFSM<Player>):Void 
+	{
+		owner.movement();
+	}
+}
+
+class Fall extends FlxFSMState<Player>
+{
+	override public function enter(owner:Player, fsm:FlxFSM<Player>):Void
+	{
+		
+	}
+	override public function update(elapsed:Float, owner:Player, fsm:FlxFSM<Player>):Void 
+	{
+		owner.movement();
+	}
+}
